bd5b327d68764ff00fd7030c426be194cf1e39f0
shoeb <shaikhshoeb772@gmail.com>
2023-05-01 17:23:45 +0530

    dark & light theme working

--- src/components/treeStructure/TreeStructure.tsx
+++ src/components/treeStructure/TreeStructure.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import 'boxicons';
 import Arrow from '../../assets/image/right-arrow.png';
 import ThemeSwitcher from '../themeSwitcher/ThemeSwitcher';
-import * as Styles from './style';
+import styled, { ThemeProvider } from 'styled-components';
 
 const files = {
   children: [
@@ -35,26 +35,93 @@ type TEntry = {
   name: string;
   children?: TEntry[];
 };
+
+interface IProps {
+  paddingLeft?: number;
+  collapse?: boolean;
+  theme?: boolean;
+}
+
+const lightTheme = {
+  colorPrimary: '#0060df',
+  colorSecondary: '#fbfbfe',
+  colorAccent: '#fd6f53',
+  fontColor: '#000000',
+  filter:
+    'invert(0%) sepia(11%) saturate(5147%) hue-rotate(194deg) brightness(96%) contrast(102%)',
+};
+
+const darkTheme = {
+  colorPrimary: '#17ed90',
+  colorSecondary: '#2a2c2d',
+  colorAccent: '#12cdea',
+  fontColor: '#ffffff',
+  filter:
+    'invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg) brightness(103%) contrast(103%)',
+};
+
+const AppContainer = styled.div<{ theme: any }>`
+  width: 240px;
+  background: ${({ theme }) => theme.colorSecondary};
+
+  height: 100vh;
+  padding: 8px;
+`;
+
+const FolderName = styled.div<{ theme: any }>`
+  line-height: 32px;
+  color: ${({ theme }) => theme.fontColor};
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  background: transparent;
+  cursor: pointer;
+  border: none;
+  display: flex;
+  align-items: center;
+  padding: 0;
+  line-height: 32px;
+`;
+
+const NestedChildren = styled.div<IProps>`
+  padding-left: ${(props) =>
+    props.paddingLeft ? `${props.paddingLeft}px` : null};
+`;
+
+const ArrowImage = styled.img<IProps>`
+  width: 10px;
+  height: 10px;
+  margin-right: 5px;
+
+  rotate: ${(props) => (props.collapse ? '90deg' : null)};
+
+  filter: ${({ theme }) => theme.filter};
+
+  /* filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg) brightness(103%)
+    contrast(103%); */
+`;
+
 const Entry = ({ entry, depth }: { entry: TEntry; depth: number }) => {
   const [collapse, setCollapse] = useState(false);
 
   return (
     <div>
       {entry.children ? (
-        <Styles.Button onClick={() => setCollapse(!collapse)}>
-          <Styles.ArrowImage collapse={collapse} src={Arrow} alt='' />
-          <Styles.FolderName>{entry.name}</Styles.FolderName>
-        </Styles.Button>
+        <Button onClick={() => setCollapse(!collapse)}>
+          <ArrowImage collapse={collapse} src={Arrow} alt='' />
+          <FolderName>{entry.name}</FolderName>
+        </Button>
       ) : (
-        <Styles.FolderName>{entry.name}</Styles.FolderName>
+        <FolderName>{entry.name}</FolderName>
       )}
 
       {collapse && (
-        <Styles.NestedChildren paddingLeft={depth * 10}>
+        <NestedChildren paddingLeft={depth * 10}>
           {entry.children?.map((entry) => (
             <Entry entry={entry} depth={depth + 1} key={entry.name} />
           ))}
-        </Styles.NestedChildren>
+        </NestedChildren>
       )}
     </div>
   );
@@ -69,12 +136,14 @@ const TreeStructure = () => {
 
   return (
     <>
-      <ThemeSwitcher handleThemeToggle={handleThemeToggle} />
-      <Styles.AppContainer theme={theme}>
-        {files.children.map((entry) => (
-          <Entry entry={entry} depth={1} key={entry.name} />
-        ))}
-      </Styles.AppContainer>
+      <ThemeProvider theme={theme ? darkTheme : lightTheme}>
+        <ThemeSwitcher handleThemeToggle={handleThemeToggle} />
+        <AppContainer>
+          {files.children.map((entry) => (
+            <Entry entry={entry} depth={1} key={entry.name} />
+          ))}
+        </AppContainer>
+      </ThemeProvider>
     </>
   );
 };
